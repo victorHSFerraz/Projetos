469ad19f716ced1866ed3efaceda59d7a0c1fcc9
Victor <victorf178@gmail.com>
2020-05-03 15:50:07 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,6 +1,3 @@
-print("Olá Mundo!")
-print("Teste")
-while True:
-    option = input("Do you want to continue? S/N: ").upper()
-    if option == "N":
-        break
+matriz = [[1, 2, 3, 4], [6, 7, 8, 9]]
+print(f"O item que esta na linha 0 e coluna 0 é {matriz[0][0]}")
+print(f"O item que esta na linha 1 e coluna 3 é {matriz[1][3]}")
