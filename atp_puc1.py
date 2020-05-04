43dd7968710530befd5422fe11278f1b1e20cb08
Victor Ferraz <victorf178@gmail.com>
2020-05-04 00:47:55 -0300

    Projeto PUC 1º Bimestre

---
+++ atp_puc1.py
@@ -0,0 +1,64 @@
+from datetime import date
+
+anoAtual = date.today().year
+
+
+# ETAPA 1
+def obter_limite():
+    input("Press Enter to Continue...")
+    print("\n\033[1;30mSeja bem-vindo! Aqui quem fala é Victor Ferraz\033[m")
+    print("\nPara prosseguir com a análise de crédito, preencha os campos abaixo.")
+    cargo = input("Cargo atual: ")
+    salario = float(input("Salário: "))
+    ano = int(input("Ano de Nascimento: "))
+    print("\n\033[32mDados Pessoais\033[m:", end=" ")
+    print("Cargo: {}".format(cargo))
+    print("\t\t\t\tSalário: R${:.2f}".format(salario))
+    print("\t\t\t\tAno de Nascimento: {}".format(ano))
+    # ETAPA 2
+    idade = anoAtual - ano
+    print("\t\t\t\tIdade: {} anos".format(idade))
+    limiteGasto = ((salario * (idade / 1000)) + 100)
+    print("\n\033[4;32mSeu limite de crédito na loja é de R${:.2f}\033[m".format(limiteGasto))
+
+    return limiteGasto, idade
+
+
+tudo = 0
+
+
+# ETAPA 3
+def verificar_produto(limiteGasto, idade):
+    global tudo
+    print("\n\033[1;30mEscolha um produto\033[m")
+    produto = input("\nNome do produto: ")
+    precoProduto = float(input("Preço do produto: "))
+    tudo = tudo + precoProduto
+    print("\nProduto: {} \nPreço: R${:.2f}".format(produto, precoProduto))
+    if precoProduto <= 0.6 * limiteGasto:
+        print("\n\033[4;32mLiberado!\033[m")
+    elif precoProduto <= 0.9 * limiteGasto:
+        print("\nLiberado ao parcelar em 2x.")
+    elif precoProduto <= limiteGasto:
+        print("\nLiberado ao parcelar em 3x ou mais.")
+    elif precoProduto > limiteGasto:
+        print("\n\033[4;31mBloqueado\033[m")
+    else:
+        print("ERRO")
+    meuNome = "Victor Ferraz"
+    primeiroNome = "Victor"
+    if len(meuNome) <= precoProduto <= idade:
+        print("\nParabéns! Você ganhou um desconto de", len(primeiroNome), "%")
+        descFinal = precoProduto * (len(primeiroNome) / 100)
+        print("Total: R${:.2f}".format(precoProduto - descFinal))
+    else:
+        print("\nTotal: R${:.2f}".format(precoProduto))
+
+
+dados = obter_limite()
+limite = dados[0]
+idade = dados[1]
+produtosQuantidade = int(input("\nQuantos produtos deseja cadastrar? "))
+for vezes in range(0, produtosQuantidade):
+    verificar_produto(limite, idade)
+print(tudo)
--- teste.py
+++
@@ -1,3 +0,0 @@
-matriz = [[1, 2, 3, 4], [6, 7, 8, 9]]
-print(f"O item que esta na linha 0 e coluna 0 é {matriz[0][0]}")
-print(f"O item que esta na linha 1 e coluna 3 é {matriz[1][3]}")
